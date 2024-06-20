067a09aeb45621031154d7c6819924e347140e45
ELYAHYAOUY-FL <imane.elyahyaouy@etu.uae.ac.ma>
2024-06-20 21:41:57 +0100

    build project

---
+++ docs/precache-manifest.76f4d1993d5a2948bc61d1951f7a6233.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f1ed35a3e6c182b613c9d2b992e72f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7828243701d1140d63a",
+    "url": "/static/css/main.0049a1d5.chunk.css"
+  },
+  {
+    "revision": "dd4ec54fd074452ca438",
+    "url": "/static/js/2.a1243b37.chunk.js"
+  },
+  {
+    "revision": "d0108935ee64eed2124abed53829b0f6",
+    "url": "/static/js/2.a1243b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7828243701d1140d63a",
+    "url": "/static/js/main.118ee91c.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "6b8d73b3aa3932cf12c948591b5d5f19",
+    "url": "/static/media/amine.6b8d73b3.jpeg"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  }
+]);
